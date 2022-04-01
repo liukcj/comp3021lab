45d9eecff91fead7320f89f19c6f187c57cad0c9
liukcj <kcliuaf@connect.ust.hk>
2022-04-01 11:18:26 +0800

    lab6 complete

---
+++ src/lab6/src/Account.java
@@ -0,0 +1,50 @@
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+
+public class Account {
+    public int id;
+    public int balance;
+
+    public Account(int id, int balance) {
+        this.id = id;
+        this.balance = balance;
+    }
+
+    // TODO: Task1
+    // replace the null with a lambda expression
+    public static Consumer<Account> add100 = x ->
+            x.balance = x.balance + 100;
+
+
+    // TODO: Task2
+    // define checkBound using lowerBound and upperBound
+    // We want checkBound to check BOTH lowerBound AND upperBound.
+    public static Predicate<Account> lowerBound = a -> a.balance >= 0;
+    public static Predicate<Account> upperBound = a -> a.balance <= 10000;
+    public static Predicate<Account> checkBound = a -> lowerBound.test(a) && upperBound.test(a);
+
+    interface AddMaker {
+        Consumer<Account> make(int N);
+    }
+
+    // TODO: Task3
+    // replace the null with a lambda expression
+    public static AddMaker maker = x -> (Account a) -> a.balance += x;
+
+
+    // You can assume that all the Account in acconts have positive balances.
+    public static int getMaxAccountID(List<Account> accounts) {
+        // TODO: Task4
+        // replace the null with a lambda expression
+        Account maxOne = accounts.stream().reduce(new Account(0, -100), (max, current) ->
+                max.balance < current.balance ? current : max
+        );
+
+        return maxOne.id;
+    }
+
+
+}
